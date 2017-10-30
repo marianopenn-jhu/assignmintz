b51049a1e1e976d01f64b829e2ebc1d497c523da
Mariano Pennini <mpennin3@jhu.edu>
2017-10-29 21:08:59 -0400

    updated resources with assignment resource and subtask resource

--- assignmintz/backend/models.py
+++ assignmintz/backend/models.py
@@ -9,5 +9,25 @@ class User(models.Model):
     passwd = models.CharField(max_length=36, default='')
     role = models.CharField(max_length=36, default='')
 
-def __str__(self):
-     return '%s %s' % (self.user_name, self.email)
+class Assignment(models.Model):
+    assignment_id = models.CharField(max_length=36, primary_key=True)
+    assignment_type = models.CharField(max_length=5, default='')
+    professor_id = models.CharField(max_length=36)
+    due_date = models.TimeField(auto_now=True)
+    expected_difficulty = models.IntegerField(default=0)
+    actual_difficulty = models.IntegerField(default=0)
+    expected_time = models.FloatField(default=0.0)
+    actual_time = models.FloatField(default=0.0)
+    priority = models.IntegerField(default=0)
+    percent_complete = models.FloatField(default=0.0)
+    visible = models.BooleanField(default=True)
+    description = models.TextField()
+
+class SubTask(models.Model):
+    subtask_id = models.CharField(max_length=36, primary_key=True)
+    subtask_name = models.CharField(max_length=36)
+    visible = models.BooleanField(default=True)
+    description = models.TextField()
+
+# def __str__(self):
+#      return '%s %s' % (self.user_name, self.email)
