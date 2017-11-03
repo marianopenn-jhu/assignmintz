7ef6fd83af3f6c092bfc26b4b10ea1829e74b3be
Mariano Pennini <mpennin3@jhu.edu>
2017-11-03 18:34:46 -0400

    modified test install

--- test-install.sh
+++ test-install.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-psql -c 'create database travis_ci_test;' -U postgres
+python3 -m venv virtualEnv
+source virtualEnv/bin/activate
+apt-get install apache2
+apt-get install libapache2-mod-wsgi-py3
 pip install -r assignmintz/assignmintz/requirements/requirements.txt
 createdb -h localhost -p 5432 -U postgres assignmintz
+source virtualEnv/bin/activate
