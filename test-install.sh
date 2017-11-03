056568af8dbd1b75c15d6f42372f5b8501c0b1ac
Mariano Pennini <mpennin3@jhu.edu>
2017-11-03 18:41:35 -0400

    removed venv commadnds

--- test-install.sh
+++ test-install.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-python3 -m venv virtualEnv
-source virtualEnv/bin/activate
 sudo apt-get install apache2
 sudo apt-get install libapache2-mod-wsgi-py3
 pip install -r assignmintz/assignmintz/requirements/requirements.txt
 createdb -h localhost -p 5432 -U postgres assignmintz
-source virtualEnv/bin/activate
