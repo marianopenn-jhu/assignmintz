76749c388372e8ac7a453cc013f398e8fa5d7676
Mariano Pennini <mpennin3@jhu.edu>
2017-11-03 18:39:33 -0400

    added sudo

--- test-install.sh
+++ test-install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 python3 -m venv virtualEnv
 source virtualEnv/bin/activate
-apt-get install apache2
-apt-get install libapache2-mod-wsgi-py3
+sudo apt-get install apache2
+sudo apt-get install libapache2-mod-wsgi-py3
 pip install -r assignmintz/assignmintz/requirements/requirements.txt
 createdb -h localhost -p 5432 -U postgres assignmintz
 source virtualEnv/bin/activate
