1e5590ea1f6a9833011924270024672667807772
Mariano Pennini <mpennin3@jhu.edu>
2017-11-03 18:55:57 -0400

    added httpd-devel

--- test-install.sh
+++ test-install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sudo apt-get install apache2
 sudo apt-get install libapache2-mod-wsgi-py3
+sudo apt-get httpd-devel
 pip install -r assignmintz/assignmintz/requirements/requirements.txt
 createdb -h localhost -p 5432 -U postgres assignmintz
