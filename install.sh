18f9d2ee56bdfd634ade775591e4ccbe571a4045
Mariano Pennini <mpennin3@jhu.edu>
2017-11-08 14:29:44 -0500

    changed requirements to remove mod_wsgi

--- install.sh
+++ install.sh
@@ -3,6 +3,4 @@ python3 -m venv virtualEnv
 source virtualEnv/bin/activate
 pip install -r assignmintz/assignmintz/requirements/requirements.txt
 createdb -h localhost -p 5432 -U postgres assignmintz
-#psql -h localhost -p 5432 -U postgres assignmintz
-#psql -d assignmintz -a -f  assignmintz/assignmintz/database/init.sql
 source virtualEnv/bin/activate
