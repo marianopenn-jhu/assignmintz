d95a4fce8ad599db975ad1059dda2048b3552a6a
Mariano Pennini <mpennin3@jhu.edu>
2017-11-09 20:08:35 -0500

    last change to install script

--- test-install.sh
+++ test-install.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-python3 -m venv virtualEnv/
-source virtualEnv/bin/activate
 pip install -r assignmintz/assignmintz/requirements/requirements.txt
 createdb -h localhost -p 5432 -U postgres assignmintz
-source virtualEnv/bin/activate
