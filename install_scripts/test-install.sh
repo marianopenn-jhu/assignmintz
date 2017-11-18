ec3695fce50778e3b2a7ef729c96f288de1edd53
Mariano Pennini <mpennin3@jhu.edu>
2017-11-18 11:53:24 -0500

    removed install dir bc of file path issues on travisci

--- install_scripts/test-install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-pip install -r ../requirements.txt
-createdb -h localhost -p 5432 -U postgres assignmintz
-python3 ../manage.py makemigrations
-python3 ../manage.py migrate
---
+++ test-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+pip install -r requirements.txt
+createdb -h localhost -p 5432 -U postgres assignmintz
+python3 manage.py makemigrations
+python3 manage.py migrate
