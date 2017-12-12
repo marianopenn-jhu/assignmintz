3800a01fb2b6f479ac03f4a2e242465e7651a3ed
Emily <emilyjohnson84@ymail.com>
2017-12-12 09:51:14 -0500

    nthn

--- frontend/src/app/scenes/Student/components/FindClassView/index.jsx
+++ frontend/src/app/scenes/Student/components/FindClassView/index.jsx
@@ -20,6 +20,73 @@ const TopBarContainer = styled.div
   background:grey;
 `;
 
+const Container = styled.div`
+  overflow:hidden;
+  overflow-y:auto;
+  height: 100%;
+  width: 80%;
+  position: absolute;
+  right: 0;
+  top: 0;
+  margin: 0;
+  padding: 0;
+  padding-top:10px;
+  background:white;
+`;
+
+const XOut = styled.span`
+  font-size:40px;
+  float:right;
+  padding-right:10%;
+
+  &:hover {
+    color:gray;
+    cursor:pointer;
+  }
+`;
+
+const Form = styled.form`
+  width:100%;
+`;
+
+const Header = styled.h1`
+  font-family:Avenir;
+  font-size:30px;
+  padding-left:50px;
+`;
+
+const ItemLabel = styled.div
+`
+  display:block;
+  width:85%;
+  padding-top:20px;
+  padding-bottom:20px;
+  text-align:center;
+`;
+
+const TextLabel = styled.div
+`
+  display:inline-block;
+  font-family:Avenir;
+  font-size:16px;
+  width:25%;
+  text-align:right;
+  padding-right:20px;
+`;
+
+const TextInput = styled.input
+`
+  display:inline-block;
+  width:70%;
+`;
+
+const BigTextInput = styled.textarea
+`
+  display:inline-block;
+  width:70%;
+  height:150px;
+  resize: none;
+`;
 
 class FindClassView extends React.Component {
   constructor(props) {
@@ -48,12 +115,20 @@ class FindClassView extends React.Component {
   render() {
     return (
         <Panel>
-        <TopBarContainer>
-          hello
-        </TopBarContainer>
-            <div>
-
-            </div>
+          <Container>
+          <XOut onClick={this.props.onClose}><FaClose/></XOut>
+          <Header>Create a Class</Header>
+          <Form onSubmit={this.handleSubmit}>
+            <ItemLabel>
+              <TextLabel>Course Title:</TextLabel>
+              <TextInput name="course_title" type="text" onChange={this.handleChange} />
+            </ItemLabel>
+            <ItemLabel>
+              <TextLabel>Course Number:</TextLabel>
+              <TextInput name="course_id" type="text" onChange={this.handleChange} />
+            </ItemLabel>
+          </Form>
+          </Container>
         </Panel>
     )
   }
