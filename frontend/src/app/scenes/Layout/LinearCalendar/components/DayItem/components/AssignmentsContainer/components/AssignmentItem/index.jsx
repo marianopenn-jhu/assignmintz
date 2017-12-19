d0786a95f7d3c794c62ecf6579cc23cd1521e19d
mattkae <mkosare1@gmail.com>
2017-12-18 20:12:49 -0500

    Added additional information to assignment entry in linear calendar

--- frontend/src/app/scenes/Layout/LinearCalendar/components/DayItem/components/AssignmentsContainer/components/AssignmentItem/index.jsx
+++ frontend/src/app/scenes/Layout/LinearCalendar/components/DayItem/components/AssignmentsContainer/components/AssignmentItem/index.jsx
@@ -18,6 +18,18 @@ const Header=styled.h1`
   font-size:15px;
 `;
 
+const InfoWrapper = styled.div`
+  margin-left:2%;
+  margin-right:2%;
+  padding-top:1%;
+  padding-bottom:1%;
+`;
+
+const Info = styled.div`
+  font-family:Avenir;
+  font-size:14px;
+`;
+
 const BodyWrapper=styled.div`
   margin-left:2%;
   margin-right:2%;
@@ -37,11 +49,19 @@ class AssignmentItem extends React.Component {
   render() {
     Moment.locale('en');
     var localText = Moment.utc(this.props.data.due_date).format('hh:mm a');
+    console.log(this.props.data);
     return(
       <Item>
         <Header>{this.props.data.assignment_name}: Due by {localText}</Header>
+        <InfoWrapper>
+          <Info><b>Course </b>{this.props.data.course}</Info>
+          <Info><b>Difficulty </b> {this.props.data.expected_difficulty} out of 10</Info>
+          <Info><b>Assignment Type </b>{this.props.data.assignment_type}</Info>
+          <Info><b>Expected Time </b>{this.props.data.expected_time}</Info>
+        </InfoWrapper>
+
         <BodyWrapper>
-          <Body>{this.props.data.description}</Body>
+          <Body><b>Description </b>{this.props.data.description}</Body>
         </BodyWrapper>
       </Item>
     )
